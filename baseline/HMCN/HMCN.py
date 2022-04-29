f619957171deab0b4b034ef71a861c25cf1d605d
tongxiao <xt20020109@mail.ustc.edu.cn>
2022-04-29 19:35:13 +0800

    HMCN hierarchy

---
+++ baseline/HMCN/HMCN.py
@@ -0,0 +1,28 @@
+'''
+@Author: Xiao Tong
+@FileName: HMCN-F.py
+@CreateTime: 2022-04-29 15:52:10
+@Description:
+
+'''
+
+import os
+import sys
+import tqdm
+import torch
+
+
+class HMCN_F(torch.nn.Module):
+    def __init__(self) -> None:
+        super(HMCN_F, self).__init__()
+
+    def forward(self, input_x):
+        pass
+
+
+class HMCN_R(torch.nn.Module):
+    def __init__(self) -> None:
+        super(HMCN_R, self).__init__()
+
+    def forward(self, input_x):
+        pass
