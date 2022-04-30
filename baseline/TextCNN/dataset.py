6a87466b8d272c294e8a883d2444245e2ed225b7
TownShaw <xt20020109@mail.ustc.edu.cn>
2022-04-30 22:23:12 +0800

    finish TextCNN, waiting for debugging

---
+++ baseline/TextCNN/dataset.py
@@ -0,0 +1,67 @@
+'''
+@Author: Xiao Tong
+@FileName: dataset.py
+@CreateTime: 2022-04-30 20:01:55
+@Description:
+
+'''
+
+import os
+import torch
+import pickle
+import numpy as np
+from torch.utils.data import Dataset
+from utils import get_stopwords, load_khan_data_by_id
+
+
+class KhanDataset(Dataset):
+    def __init__(self, config: dict, fileids: list, word2idx: dict) -> None:
+        super(KhanDataset, self).__init__()
+        self.config = config
+        self.word2idx = word2idx
+        self.id2labels = pickle.load(open(config["data"]["id2labels"], "rb"))
+        # delete fileids that not in id2labels, which means delete data that doesn't have a label
+        self.fileids = list(set(fileids).intersection(set(self.id2labels.keys())))
+        self.stopwords = get_stopwords(self.config["data"]["stopwords"])
+        self.num_classes = self.config["data"]["num_classes"]
+
+    def __getitem__(self, idx):
+        fileid = self.fileids[idx]
+        filepath = os.path.join(self.config["data"]["sample_dir"], fileid, fileid + ".keyframes.pkl")
+        images, subtitles, lens = load_khan_data_by_id(filepath,
+                                                       self.word2idx,
+                                                       self.stopwords,
+                                                       max_seq_len=self.config["model"]["max_seq_len"],
+                                                       pad_word=self.config["model"]["pad_word"])
+
+        label = self.id2labels[fileid]
+        multi_hot_label = torch.FloatTensor([1. if i in label else 0. for i in range(self.num_classes)])
+        return images, subtitles, lens, multi_hot_label
+
+    def __len__(self):
+        return len(self.fileids)
+
+
+def collate_fn(batch_data):
+    batch = {"images": [], "subtitles": [], "lens": [], "labels": [], "segments": [], "image_segments": []}
+    for data in batch_data:
+        images, subtitles, lens, labels = data
+        batch["labels"].append(labels)
+        image_segment = [len(section_images) for section_images in images]
+        segment = [len(section_subtitles) for section_subtitles in subtitles]
+        batch["image_segments"].append(image_segment)
+        batch["segments"].append(segment)
+        images = [torch.from_numpy(np.array(image, dtype=np.float32)) for section_images in images for image in section_images]
+        images = torch.stack(images, dim=0).transpose(1, 3)
+        subtitles = [torch.LongTensor(subtitle) for section_subtitles in subtitles for subtitle in section_subtitles]
+        subtitles = torch.stack(subtitles, dim=0)
+        lens = [length for section_lens in lens for length in section_lens]
+        lens = torch.LongTensor(lens)
+        batch["images"].append(images)
+        batch["subtitles"].append(subtitles)
+        batch["lens"].append(lens)
+    batch["images"] = torch.cat(batch["images"], dim=0)
+    batch["subtitles"] = torch.cat(batch["subtitles"], dim=0)
+    batch["lens"] = torch.cat(batch["lens"], dim=0)
+    batch["labels"] = torch.stack(batch["labels"], dim=0)
+    return batch
---
+++ baseline/TextCNN/model.py
@@ -0,0 +1,79 @@
+'''
+@Author: Xiao Tong
+@FileName: model.py
+@CreateTime: 2022-04-30 20:01:50
+@Description:
+
+'''
+
+import torch
+import torch.nn.functional as F
+from torch.nn.init import xavier_normal_
+from torchvision.models import resnet34
+
+
+class EmbeddingLayer(torch.nn.Module):
+    def __init__(self, word_num, embedding_dim, pretrained_word_matrix=None):
+        super(EmbeddingLayer, self).__init__()
+        self.embedding = torch.nn.Embedding(word_num, embedding_dim)
+        if pretrained_word_matrix is not None:
+            self.embedding.weight.data = torch.from_numpy(pretrained_word_matrix)
+        else:
+            xavier_normal_(self.embedding.weight.data)
+
+    def forward(self, input_x):
+        return self.embedding(input_x)
+
+
+class TextCNN(torch.nn.Module):
+    def __init__(self, out_channel, word_embedding_dim, kernel_size_list, dropout):
+        super(TextCNN, self).__init__()
+        self.out_channel = out_channel
+        self.word_embedding_dim = word_embedding_dim
+        self.kernel_size_list = kernel_size_list
+        self.convs = [torch.nn.Conv2d(1, out_channel, kernel_size=(w, word_embedding_dim)) for w in kernel_size_list]
+        self.convs = torch.nn.ModuleList(self.convs)
+        self.dropout = torch.nn.Dropout(dropout)
+
+    def forward(self, input_x):
+        convs_out = [conv(input_x) for conv in self.convs]
+        pool_x = [F.max_pool1d(x.squeeze(-1), x.size()[2]) for x in convs_out]
+        fc_x = torch.cat(pool_x, dim=1)
+        fc_x = fc_x.squeeze(-1)
+        fc_x = self.dropout(fc_x)
+        return fc_x
+
+
+class ResNetTextCNN(torch.nn.Module):
+    def __init__(self, config, num_words, pretrained_word_embedding=None):
+        super(ResNetTextCNN, self).__init__()
+        self.embedding = EmbeddingLayer(num_words,
+                                        config["model"]["word_embedding_dim"],
+                                        pretrained_word_embedding)
+
+        self.resnet = resnet34(pretrained=True, progress=True)
+        self.textcnn = TextCNN(config["model"]["out_channel"],
+                               config["model"]["word_embedding_dim"],
+                               config["model"]["kernel_size"],
+                               config["model"]["dropout"])
+
+        self.resnet_outdim = self.resnet.fc.out_features
+        self.linear_out = torch.nn.Linear(self.resnet_outdim + config["model"]["kernel_size"] * config["model"]["out_channel"],
+                                          config["data"]["num_classes"])
+
+    def forward(self, image_input, input_x, segments):
+        image_output = self.resnet(image_input)
+
+        embedding_out = self.embedding(input_x)
+        text_output = self.textcnn(embedding_out.unsqueeze(1))
+        logits = self.linear_out(torch.cat([image_output, text_output], dim=-1))
+        scores = torch.sigmoid(logits)
+
+        start_idx, end_idx = 0, 0
+        video_scores = []
+        for video in segments:
+            start_idx = end_idx
+            end_idx += sum(video)
+            video_scores.append(torch.max(scores[start_idx:end_idx], dim=0)[0])
+        video_scores = torch.stack(video_scores, dim=0)
+        return video_scores
